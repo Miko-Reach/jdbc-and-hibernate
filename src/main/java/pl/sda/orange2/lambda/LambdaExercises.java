804f7d4e15b458534358dee031b4ceceba2f7ba0
miko_reach <mikolaj.metzner@gmail.com>
2023-03-04 13:49:39 +0100

    [Change] - anonymous classes examples

--- src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
+++ src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
@@ -1,5 +1,7 @@
 package pl.sda.orange2.lambda;
 
+import java.awt.*;
+
 public class LambdaExercises {
 
     public static void main(String[] args) {
@@ -10,6 +12,20 @@ public class LambdaExercises {
 
         ChłopakiNiePłaczą boy2 = new ChłopakiNiePłaczą();
         boy2.sing();
+
+
+        //Poniżej klasa bez nazwy
+        BoysDontCry anonymousClassReference = new BoysDontCry() {
+            @Override
+            public void silnoreki() {
+
+                System.out.println("ANON SILNOREKI");
+            }
+        };
+
+        anonymousClassReference.silnoreki();
+
+      //  BoysDontCry stupid = new String("")
     }
 }
 
