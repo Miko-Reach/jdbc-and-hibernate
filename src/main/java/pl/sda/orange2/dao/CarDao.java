a33b5514c0ad14a42e2b412df0d831b838682766
miko_reach <mikolaj.metzner@gmail.com>
2023-03-07 21:06:41 +0100

    [added]  class CarDao and Id parameters

---
+++ src/main/java/pl/sda/orange2/dao/CarDao.java
@@ -0,0 +1,28 @@
+package pl.sda.orange2.dao;
+
+import pl.sda.orange2.Entity.Car;
+
+
+import java.util.List;
+
+
+
+    public class CarDao {
+
+        public void save(Car car) {
+
+        }
+
+        public List<Car> findAll() {
+            return List.of();
+        }
+
+        public Car findById(Long id) {
+            return null;
+        }
+
+        public void deleteById(Long id) {
+
+        }
+    }
+
