177ddf265f9f3d8c89b60aacb5f132cb5e83e374
miko_reach <mikolaj.metzner@gmail.com>
2023-03-07 19:16:21 +0100

    [Change]  Comitted changes in DB config so far empty class

---
+++ src/main/java/pl/sda/orange2/enumerable/DBConfig.java
@@ -0,0 +1,9 @@
+package pl.sda.orange2.enumerable;
+
+
+public enum DBConfig {
+    DB_URL,
+    USER,
+    PASSWORD
+}
+
--- src/main/java/pl/sda/orange2/jdbc/TestConnection.java
+++ src/main/java/pl/sda/orange2/jdbc/TestConnection.java
@@ -9,6 +9,7 @@ import java.sql.Statement;
 
 public class TestConnection {
 
+    String className =TestConnection.class.getName();
     public static void main(String[] args) {
 
         String query = """
